16ad900598e7aeb809131ddd38ab6c19d9c5fc32
Ivan Tan <ivantjh97@gmail.com>
2019-07-16 10:03:35 +0800

    Reorganize structure

---
+++ bot.py
@@ -0,0 +1,88 @@
+import json
+import logging
+from os import environ
+from queue import Queue
+from threading import Thread
+
+from telegram import Bot
+from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Filters
+from db import *
+
+TOKEN = environ['TELEGRAM_TOKEN']
+CREDS = environ['GOOGLE_APPLICATION_CREDENTIALS']
+
+db = Db(json.loads(CREDS))
+
+logger = logging.getLogger('quote_bot')
+
+def start(bot, update):
+    update.message.reply_text('Hi, let\'s save some quotes!')
+
+# Only for telegram errors
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+def save_quote(bot, update):
+    message_to_quote = update.message.reply_to_message
+
+    if message_to_quote:
+        if message_to_quote.text:
+            chat_id = message_to_quote.chat.id
+            username = message_to_quote.from_user.username
+            text, datetime = message_to_quote.text, message_to_quote.date
+            
+            db.save_quote(chat_id, text, username, datetime)
+            update.message.reply_text('Message saved to quotes', reply_to_message_id=message_to_quote.message_id)
+            logger.info(f'Saved "{text}" from {username} to chat {chat_id}')
+
+        else:
+            update.message.reply_text('Sorry, you can only save text messages.')
+
+    else:
+        update.message.reply_text('Reply this command to the message you would like to quote.')
+
+def rand_quote(bot, update):
+    quote = db.rand_quote(update.message.chat_id)
+
+    if not quote:
+        update.message.reply_text('No quotes yet. Trying saving some quotes!')
+        logger.info(f'Retrieved no msg for chat {update.message.chat_id}')
+
+    else:
+        date = quote["datetime"].strftime('%d/%m/%Y')
+        update.message.reply_text(f'"{quote["msg"]}" \u2014 @{quote["user"]} on {date}')
+        logger.info(f'Retrieved random msg "{quote["msg"]}" from {quote["user"]} to chat {update.message.chat_id}')
+
+# def run(updater):
+#     if MODE == 'prod':
+#         HEROKU_APP_NAME = environ['HEROKU_APP_NAME']
+#         updater.start_webhook(listen='0.0.0.0', port=PORT, url_path=TOKEN)
+#         updater.bot.set_webhook(f'https://{HEROKU_APP_NAME}.herokuapp.com/{TOKEN}')
+#         logger.info('Running server on production')
+
+#     elif MODE == 'dev':
+#         updater.start_polling()
+#         logger.info('Running server on development')
+
+def setup():
+    # Create bot, update queue and dispatcher instances
+    bot = Bot(TOKEN)
+    update_queue = Queue()
+    
+    dp = Dispatcher(bot, update_queue)
+    
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('quote', save_quote))
+    dp.add_handler(CommandHandler('random', rand_quote)) 
+    
+    # Start the thread
+    thread = Thread(target=dp.start, name='dispatcher')
+    thread.start()
+
+    logger.info('Bot thread started')
+    
+    # you might want to return dispatcher as well, 
+    # to stop it at server shutdown, or to register more handlers:
+    # return (update_queue, dispatcher) 
+    return (update_queue, dp)
--- main.py
+++ main.py
@@ -1,94 +1,25 @@
 import logging
-import json
-
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from dotenv import load_dotenv
 from os import environ
-from db import *
+
+from flask import Flask
+from dotenv import load_dotenv
+
+from bot import setup
 
 load_dotenv()
-TOKEN = environ['TELEGRAM_TOKEN']
-CREDS = environ['GOOGLE_APPLICATION_CREDENTIALS']
 MODE = environ.get('MODE', 'dev')
 PORT = int(environ.get('PORT', '8443'))
-
-db = Db(json.loads(CREDS))
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger('quote_bot')
 
-def start(bot, update):
-    update.message.reply_text('Hi, let\'s save some quotes!')
+update_queue, dispatcher = setup()
 
-# Only for telegram errors
-def error(bot, update, error):
-    logger.warning('Update "%s" caused error "%s"', update, error)
+app = Flask(__name__)
 
-def save_quote(bot, update):
-    message_to_quote = update.message.reply_to_message
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
-    if message_to_quote:
-        if message_to_quote.text:
-            chat_id = message_to_quote.chat.id
-            username = message_to_quote.from_user.username
-            text, datetime = message_to_quote.text, message_to_quote.date
-            
-            db.save_quote(chat_id, text, username, datetime)
-            update.message.reply_text('Message saved to quotes', reply_to_message_id=message_to_quote.message_id)
-            logger.info(f'Saved "{text}" from {username} to chat {chat_id}')
-
-        else:
-            update.message.reply_text('Sorry, you can only save text messages.')
-
-    else:
-        update.message.reply_text('Reply this command to the message you would like to quote.')
-
-def rand_quote(bot, update):
-    quote = db.rand_quote(update.message.chat_id)
-
-    if not quote:
-        update.message.reply_text('No quotes yet. Trying saving some quotes!')
-        logger.info(f'Retrieved no msg for chat {update.message.chat_id}')
-
-    else:
-        date = quote["datetime"].strftime('%d/%m/%Y')
-        update.message.reply_text(f'"{quote["msg"]}" \u2014 @{quote["user"]} on {date}')
-        logger.info(f'Retrieved random msg "{quote["msg"]}" from {quote["user"]} to chat {update.message.chat_id}')
-
-def run(updater):
-    if MODE == 'prod':
-        HEROKU_APP_NAME = environ['HEROKU_APP_NAME']
-        updater.start_webhook(listen='0.0.0.0', port=PORT, url_path=TOKEN)
-        updater.bot.set_webhook(f'https://{HEROKU_APP_NAME}.herokuapp.com/{TOKEN}')
-        logger.info('Running server on production')
-
-    elif MODE == 'dev':
-        updater.start_polling()
-        logger.info('Running server on development')
-
-def main():
-    # Create the EventHandler and pass it your bot's token.
-    updater = Updater(token=TOKEN)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(CommandHandler('quote', save_quote))
-    dp.add_handler(CommandHandler('random', rand_quote))
-    
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    run(updater)
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
-if __name__ == '__main__':
-    logger.info('Starting bot')
-    main()
+logger.info('Starting web server')
